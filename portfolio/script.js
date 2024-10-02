e1ded0cf9051be824e50376310c7715919e7b87d
NOUTAILAA <notaila7@gmail.com>
2024-10-02 19:11:13 +0100

    first commit

---
+++ portfolio/script.js
@@ -0,0 +1,20 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const sections = document.querySelectorAll('section');
+    const options = {
+        root: null,
+        threshold: 0.1,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+            }
+        });
+    }, options);
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+});
